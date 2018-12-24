b9cdc9031625b841806e5bc1552aa06f0927245d
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2018-12-24 18:50:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include  <stdio.h>
 #include<stdlib.h>
+#define NUM_DATA 100
 void RadixSort(int x[], int n, int r);
 void ShowData(int x[], int n);
-void RadixSort1(int x[], int n, int r);
-void ShowData1(int x[], int n);
-#define NUM_DATA 100
 
 static int rad[NUM_DATA];    
 static int y[NUM_DATA];      
@@ -34,30 +32,6 @@ void RadixSort(int x[], int n, int r)
 		m *= 10;                    
 	}
 }
-void RadixSort1(int x[], int n, int r)  
-{                                       
-	int i, j, k;                      
-	int m = 1;                         
-
-	while (m <= r) {
-		for (i = 0; i < n; i++)
-			rad[i] = (x[i] / m) % 10;   
-
-		k = 0;                          
-		for (i = 0; i <= 9; i++)        
-			for (j = 0; j < n; j++)
-				if (rad[j] == i)        
-					y[k++] = x[j];      
-
-		for (i = 0; i < n; i++)
-			x[i] = y[i];      
-
-		ShowData1(x, n);
-		printf("\n");
-		m *= 10;                      
-	}
-}
-
 
 void ShowData(int x[], int n)
 {
@@ -69,16 +43,7 @@ void ShowData(int x[], int n)
 			printf(" ");
 	}
 }
-void ShowData1(int x[], int n)
-{
-	int i;
 
-	for (i = 0; i < n; i++) {
-		printf("0%d", x[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-}
 
 int main(int argc, char *argv[])
 {
@@ -93,20 +58,14 @@ int main(int argc, char *argv[])
 		int n = count;       
 		int r = 100;     
 		
-		if (a == 321 || a == 762) {			   
+		if (x[count - 1] == 12||a == 321 || a == 762) {
 		
 			ShowData(x, n);
 			printf("\n");
 
 			RadixSort(x, n, r);
-		}
-		if (x[count - 1] == 12) {			   
-			
-			ShowData1(x, n);
-			printf("\n");
 
-			RadixSort1(x, n, r);
 		}
 	}
-    return 0;
+	return 0;
 }
